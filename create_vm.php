ef39e4486be0ccd240d56ae7bc1ae95190d5db3e
jopa <106079878+PajaStef@users.noreply.github.com>
2025-02-15 11:15:40 +0100

    Update create_vm.php

--- create_vm.php
+++ create_vm.php
@@ -1,13 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $template = escapeshellarg($_POST["template"]);	
+    $template = escapeshellarg($_POST["template"]);
     $vmname = escapeshellarg($_POST["vmname"]);
     $specs = $_POST["specs"];
 
     list($cores, $ram) = explode('_', $specs);
 
     // Path to your bash script
-    $script = '/var/www/html/start.sh'; //path to the script that creates the vm
+    $script = '/var/www/html/create_vm.php'; //path to the script that creates the vm
 
     // Execute the bash script with the provided parameters
     $command = "bash $script $template $vmname $cores $ram";
